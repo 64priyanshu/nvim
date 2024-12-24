d81ac2577c9b1bf8e86fe1a07fc3869e521846e6
priyanshu <priyanshukatuwal@gmail.com>
2024-12-24 08:00:34 +0545

    added more useful autocommands

--- lua/priyanshu/core/autocommands.lua
+++ lua/priyanshu/core/autocommands.lua
@@ -19,3 +19,22 @@ vim.api.nvim_create_autocmd("VimEnter", {
 		print("Startup time: " .. string.format("%.3f ms", elapsed))
 	end,
 })
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+-- go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			pcall(vim.api.nvim_win_set_cursor, 0, mark)
+		end
+	end,
+})
