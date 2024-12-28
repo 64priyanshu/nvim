cbac771f90d88d086af836a4de0525dd2fa2b93f
priyanshu <priyanshukatuwal@gmail.com>
2024-12-28 18:27:44 +0545

    added and changed lspconfig keybindings

--- lua/priyanshu/plugins/lsp/lspconfig.lua
+++ lua/priyanshu/plugins/lsp/lspconfig.lua
@@ -7,12 +7,14 @@ local M = {
 function M.config()
 	local on_attach = function(client, bufnr)
 		local bufopts = { noremap = true, silent = true, buffer = bufnr }
-		vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, bufopts)
-		vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, bufopts)
-		vim.keymap.set("n", "<leader>lr", vim.lsp.buf.rename, bufopts)
-		vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, bufopts)
-		vim.keymap.set("n", "<leader>gr", require("telescope.builtin").lsp_references, bufopts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+		vim.keymap.set("n", "<leader>lr", require("telescope.builtin").lsp_references, bufopts)
+		vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, bufopts)
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+		vim.keymap.set({ "n", "i" }, "<C-h>", vim.lsp.buf.signature_help, bufopts)
 	end
 
 	local capabilities_ = vim.lsp.protocol.make_client_capabilities()
