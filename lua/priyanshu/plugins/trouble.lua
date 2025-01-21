85b6a40a404f78a8f1f55a83bc64569179e6a7fd
Priyanshu Katuwal <priyanshukatuwal@gmail.com>
2025-01-21 11:02:48 +0545

    fix highlight issue in trouble and added few opts

--- lua/priyanshu/plugins/trouble.lua
+++ lua/priyanshu/plugins/trouble.lua
@@ -1,7 +1,12 @@
 local M = {
 	"folke/trouble.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {},
+	opts = {
+		focus = true,
+		auto_close = true,
+		vim.api.nvim_set_hl(0, "TroubleNormal", { bg = "#282828" }),
+		vim.api.nvim_set_hl(0, "TroubleNormalNC", { link = "TroubleNormal" }),
+	},
 	cmd = "Trouble",
 	keys = {
 		{ "<leader>tt", "<CMD>Trouble diagnostics toggle<CR>" },
