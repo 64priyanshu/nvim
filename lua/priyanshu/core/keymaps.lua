4daab09153f40ca8fd770ddf6001318cdf910bfb
Priyanshu Katuwal <priyanshukatuwal@gmail.com>
2025-01-24 15:54:40 +0545

    modified qf toggle keymap

--- lua/priyanshu/core/keymaps.lua
+++ lua/priyanshu/core/keymaps.lua
@@ -91,11 +91,19 @@ vim.keymap.set("n", "<leader>t|", ":botright 70vsplit | term<CR>", { silent = tr
 vim.keymap.set("n", "]q", "<CMD>cnext<CR>", { silent = true })
 vim.keymap.set("n", "[q", "<CMD>cprev<CR>", { silent = true })
 
--- Toggle quickfix window
+-- Toggle quickfix window (Doesn't open if qf is empty)
 vim.keymap.set("n", "<leader>tq", function()
-	if vim.fn.getwininfo(vim.fn.win_getid())[1].quickfix == 1 then
+	local qf_exists = false
+	for _, win in pairs(vim.fn.getwininfo()) do
+		if win["quickfix"] == 1 then
+			qf_exists = true
+		end
+	end
+	if qf_exists == true then
 		vim.cmd("cclose")
-	else
+		return
+	end
+	if not vim.tbl_isempty(vim.fn.getqflist()) then
 		vim.cmd("copen")
 	end
 end, { silent = true })
