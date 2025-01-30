d49d16e94e5edded525e4c190d8a2a34b688ebdb
Priyanshu Katuwal <priyanshukatuwal@gmail.com>
2025-01-30 09:58:15 +0545

    updated log autocmd

--- lua/priyanshu/core/autocommands.lua
+++ lua/priyanshu/core/autocommands.lua
@@ -88,7 +88,7 @@ vim.api.nvim_create_autocmd("TermOpen", {
 
 -- Set up logdir and logfile paths
 local logdir = vim.fn.expand("$HOME") .. "/.vim/logdir"
-local logfile = logdir .. "/logs.txt"
+local logfile = logdir .. "/log.txt"
 
 -- Create logdir if it doesn't exist
 vim.fn.mkdir(logdir, "p")
@@ -96,8 +96,8 @@ vim.fn.mkdir(logdir, "p")
 -- Autocommand to re-write (not append) vim messages in logs.txt on exiting
 vim.api.nvim_create_autocmd("VimLeavePre", {
 	callback = function()
-		vim.cmd(string.format("silent! redir! > %s", logfile))
-		vim.cmd("silent! messages")
-		vim.cmd("silent! redir END")
+		local messages = vim.fn.execute("messages")
+		messages = messages:gsub("^\n+", ""):gsub("\n+$", "")
+		vim.fn.writefile(vim.split(messages, "\n"), logfile)
 	end,
 })
