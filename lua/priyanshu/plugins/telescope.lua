aadd41ee2d62bb7752ff118ac3c50f589a9a29da
Priyanshu Katuwal <priyanshukatuwal@gmail.com>
2025-02-28 10:08:07 +0545

    telescope fixed live_grep searching .git contents

--- lua/priyanshu/plugins/telescope.lua
+++ lua/priyanshu/plugins/telescope.lua
@@ -15,7 +15,7 @@ function M.config()
 	require("telescope").setup({
 		pickers = {
 			live_grep = {
-				file_ignore_patterns = { "node_modules", "%.git/", ".venv" },
+				file_ignore_patterns = { "node_modules", ".git", ".venv" },
 				additional_args = function(_)
 					return { "--hidden" }
 				end,
