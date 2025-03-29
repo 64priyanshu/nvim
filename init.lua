d66d3c113b4619d1645cdd09caa291aa42cd5a42
Priyanshu Katuwal <priyanshukatuwal@gmail.com>
2025-03-29 14:46:42 +0545

    removed vim-log

--- init.lua
+++ init.lua
@@ -6,7 +6,6 @@ require("priyanshu.core.autocommands")
 -- Extras utilitis
 require("priyanshu.core.extras.cwd-persistent")
 require("priyanshu.core.extras.session")
-require("priyanshu.core.extras.vim-log")
 
 -- Lazy
 require("priyanshu.lazy")
