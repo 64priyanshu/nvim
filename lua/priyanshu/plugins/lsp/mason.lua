cda79162db770c1a7e67744bae8a4eaead2c34b2
Priyanshu Katuwal <priyanshukatuwal@gmail.com>
2025-04-14 11:02:11 +0545

    removed mason-lspconfig

--- lua/priyanshu/plugins/lsp/mason.lua
+++ lua/priyanshu/plugins/lsp/mason.lua
@@ -1,29 +1,23 @@
 local M = {
 	"williamboman/mason.nvim",
 	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
 		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
 }
 
 function M.config()
 	require("mason").setup()
-	require("mason-lspconfig").setup({
-		ensure_installed = {
-			"lua_ls",
-			"clangd",
-			"ts_ls",
-			"html",
-			"cssls",
-			"emmet_language_server",
-		},
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	})
 	require("mason-tool-installer").setup({
 		ensure_installed = {
+			-- LSPs
+			"clangd",
+			"css-lsp",
+			"emmet-language-server",
+			"html-lsp",
+			"intelephense",
+			"lua-language-server",
+			"typescript-language-server",
+			-- Others
 			"prettier",
 			"stylua",
 		},
