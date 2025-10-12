a60035da9c6478c186a448c3c3ee3e191dd1f7da
Priyanshu Katuwal <priyanshukatuwal@gmail.com>
2025-10-12 12:47:08 +0545

    refactor!: remove extra utilities

--- init.lua
+++ init.lua
@@ -3,12 +3,6 @@ require("priyanshu.core.options")
 require("priyanshu.core.keymaps")
 require("priyanshu.core.autocommands")
 
--- Extras utilitis
-require("priyanshu.core.extras.cwd-persistent")
-require("priyanshu.core.extras.session")
-require("priyanshu.core.extras.view")
-require("priyanshu.core.extras.yank-ring")
-
 -- Lazy
 require("priyanshu.lazy")
 
