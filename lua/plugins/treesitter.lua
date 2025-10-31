3ae7fb9782ff2dfb9dd54ca4734eebac84bc129d
Priyanshu Katuwal <priyanshukatuwal@gmail.com>
2025-10-31 19:24:42 +0545

    chore(treesitter): remove build from vim.pack as it doesn't work

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 vim.pack.add({
-  { src = "https://github.com/nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 })
 
 require("nvim-treesitter.configs").setup({
